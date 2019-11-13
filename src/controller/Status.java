1215c991553e3f0c4bea906c4edd722f3c1eb0ed
wndnjs9878 <2017112064@dongguk.edu>
2019-11-13 14:31:30 +0900

    jw

--- src/controller/Status.java
+++ src/controller/Status.java
@@ -121,7 +121,6 @@ public class Status extends HttpServlet {
 			RequestDispatcher dispatcher=request.getRequestDispatcher(page);
 			dispatcher.forward(request, response);   
 
-
 			DBmanager.close(conn);
 
 		}catch (Exception e)
